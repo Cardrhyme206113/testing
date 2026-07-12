6237014646cd9cd9686d877cb5cb2623b1d4d190
Cardrhyme <70390455+Cardrhyme206113@users.noreply.github.com>
2026-07-12 16:56:33 +0300

    Bump SharpLayer preflight build to v0.4

--- SharpLayerAndroid/app/build.gradle.kts
+++ SharpLayerAndroid/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.cardrhyme.sharplayer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0-option-d-fast"
+        versionCode = 4
+        versionName = "0.4.0-option-d-preflight"
     }
 
     compileOptions {
