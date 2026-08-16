3f08f16ef614f175d3aa71ac268e396c43025438
Cardrhyme <70390455+Cardrhyme206113@users.noreply.github.com>
2026-08-16 14:31:45 +0300

    Fix Fabric 26.2 key mapping package

--- equirectshot/src/client/java/dev/cardrhyme/equirectshot/EquirectShotClient.java
+++ equirectshot/src/client/java/dev/cardrhyme/equirectshot/EquirectShotClient.java
@@ -3,7 +3,7 @@ package dev.cardrhyme.equirectshot;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyMappingHelper;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
